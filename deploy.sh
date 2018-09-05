c944198d47221ddaf41b5f866be296944641e377
Walentin Lamonos <walentinlamonos@googlemail.com>
2018-09-05 14:18:08 +0200

    Updated deploy.sh to force overwrite of remote code

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ssh root@cm2.network << EOF
   docker start apmt
-  docker exec apmt bash -c 'cd /usr/src/ScaleX-tune && git pull'
+  docker exec apmt bash -c 'cd /usr/src/ScaleX-tune && git fetch --all'
+  docker exec apmt bash -c 'cd /usr/src/ScaleX-tune && git reset --hard origin/master'
   docker exec apmt bash -c 'cd /usr/src/ScaleX-tune && pip install -r requirements.txt'
   docker exec apmt bash -c 'cd /usr/src/ScaleX-tune/tune && ./tunex.py restart'
 EOF
