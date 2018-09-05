a5ca4899d740a16cde590b63ad856373233cce39
Walentin Lamonos <walentinlamonos@googlemail.com>
2018-09-05 13:46:39 +0200

    More development

--- tune/tunex.py
+++ tune/tunex.py
@@ -4,7 +4,7 @@ import sys, time
 from daemon import Daemon
 from pymongo import MongoClient
 
-class TuneDaemon(Daemon):
+class TunexDaemon(Daemon):
 	def run(self):
 		while True:
 			time.sleep(1)
@@ -14,7 +14,7 @@ class MongoDatabase():
 		self.client = MongoClient('localhost', 27017)
 
 if __name__ == "__main__":
-	daemon = TuneDaemon('/tmp/tune-daemon.pid')
+	daemon = TunexDaemon('/tmp/tunex-daemon.pid')
 	mongodb = MongoDatabase()
 	if len(sys.argv) == 2:
 		if 'start' == sys.argv[1]:
@@ -25,13 +25,20 @@ if __name__ == "__main__":
 			daemon.restart()
 		elif 'setup' == sys.argv[1]:
 			print '"%s setup" requires exactly 1 argument\n'
-			print 'Usage: %s setup [username]'
+			print 'Usage: %s setup [USERNAME]\n' % sys.argv[0]
+			print 'Fetch the AWS data from the ScaleX database for [USERNAME]'
 		elif 'cluster' == sys.argv[1]:
-			print 'Usage: %s cluster status|run|remove|change' % sys.argv[0]
+			print 'Usage: %s cluster COMMAND' % sys.argv[0]
 		else:
 			print "Unknown command"
 			sys.exit(2)
 		sys.exit(0)
 	else:
-		print 'Usage: %s COMMAND' % sys.argv[0]
+		print 'Usage: %s COMMAND\n' % sys.argv[0]
+		print 'Commands: '
+		print '  start		Starts the tunex-daemon'
+		print '  stop		Stops the tunex-daemon'
+		print '  restart	Restarts the tunex-daemon'
+		print '  setup		Fetches AWS information from the ScaleX database'
+		print '  cluster	Controls and Creates AWS autoscaling clusters'
 		sys.exit(2)
