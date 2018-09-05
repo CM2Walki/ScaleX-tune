cce311ac7cdba2f581834c000bb49f3c9cf0cad2
Walentin Lamonos <walentinlamonos@googlemail.com>
2018-09-05 16:53:29 +0200

    Refactored daemon

--- tunex/tune.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client['dbUsersData']
-collection = db['usersData']
-
-cursor = collection.find({"userInfo.username" : "Walki"})
-for row in cursor:
-    print row["userInfo"]["username"]
