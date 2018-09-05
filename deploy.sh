4eeacbcaecf224487147c7cafcf8079d12d8c579
Walentin Lamonos <walentinlamonos@googlemail.com>
2018-09-05 15:12:49 +0200

    Modifications to deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ ssh root@cm2.network << EOF
   docker exec apmt bash -c 'cd /usr/src/ScaleX-tune && git reset --hard origin/master'
   docker exec apmt bash -c 'cd /usr/src/ScaleX-tune && pip install -r requirements.txt'
   docker exec apmt bash -c 'cd /usr/src/ScaleX-tune/tune && chmod +x tunex.py'
-  docker exec apmt bash -c 'cd /usr/src/ScaleX-tune/tune && ./tunex.py restart'
+  docker exec apmt bash -c 'cd /usr/src/ScaleX-tune/tune && pyinstaller --onefile tunex.py'
+  docker exec apmt bash -c 'mv /usr/src/ScaleX-tune/tune/build/tunex && /usr/bin/tunex'
+  docker exec apmt bash -c 'cd /usr/src/ScaleX-tune/tune && tunex restart'
   echo 'Deployment script done.'
 EOF
