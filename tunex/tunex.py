8412a9261a0fe7c73fe0146d9f39a3e070650d00
Walentin Lamonos <walentinlamonos@googlemail.com>
2018-09-06 17:51:40 +0200

    Test

--- tunex/tunex.py
+++ tunex/tunex.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
             if data == ':CODE:':
                 print data
                 print 'We are through!'
-                client.send('self.commandList.setupUser(%s)' % sys.argv[2])
+                client.send('self.commandList.setupUser("%s")' % sys.argv[2])
                 data = client.recv(2048)
                 print data
                 client.close()
