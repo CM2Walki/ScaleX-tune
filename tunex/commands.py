c40154fe3a122100c8e71947084d4ff0930dd779
Walentin Lamonos <walentinlamonos@googlemail.com>
2018-09-06 12:32:27 +0200

    Test

--- tunex/commands.py
+++ tunex/commands.py
@@ -13,14 +13,19 @@ class Commands:
     def setupUser(self, username):
         result = self.mongodbORM.get_user_info_from_name(username)
         if result is not None:
-            self.userStorage.set_username(result["username"])
-            self.userStorage.set_awssecret(result["awssecret"])
-            self.userStorage.set_awstoken(result["awstoken"])
-            self.userStorage.set_awsregion(result["awsregion"])
-            self.userStorage.set_awspubkeyname(result["awskeyname"])
-            self.userContext = Context(self.userStorage.get_awssecret(),
-                                       self.userStorage.get_awstoken())
-            print 'User setup successful detected X running clusters'
+            if result["username"] and result["awssecret"] and result["awstoken"] and result["awsregion"] and result["awskeyname"]:
+                self.userStorage.set_username(result["username"])
+                self.userStorage.set_awssecret(result["awssecret"])
+                self.userStorage.set_awstoken(result["awstoken"])
+                self.userStorage.set_awsregion(result["awsregion"])
+                self.userStorage.set_awspubkeyname(result["awskeyname"])
+                self.userContext = Context(self.userStorage.get_awssecret(),
+                                           self.userStorage.get_awstoken(),
+                                           self.userStorage.get_awsregion())
+                print 'User setup successful detected X running clusters'
+            else:
+                print 'User setup not complete in ScaleX Database'
+                sys.exit(2)
         else:
             print 'User setup failed! Username not found in ScaleX Database'
             sys.exit(2)
--- tunex/context.py
+++ tunex/context.py
@@ -4,10 +4,11 @@ import boto3
 
 
 class Context:
-    def __init__(self, awssecret, awstoken):
+    def __init__(self, awssecret, awstoken, awsregion):
         self.boto3 = boto3.client('ec2',
                                   aws_access_key_id=awstoken,
-                                  aws_secret_access_key=awssecret)
+                                  aws_secret_access_key=awssecret,
+                                  region_name = awsregion)
         self.clusterlist = []
         self.build_context()
 
