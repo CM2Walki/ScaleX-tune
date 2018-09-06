37bc190966c9f161d05818bd01ae58e13aac0ce0
Walentin Lamonos <walentinlamonos@googlemail.com>
2018-09-06 12:14:54 +0200

    Addded context and cluster class

--- tunex/commands.py
+++ tunex/commands.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import sys
+from context import Context
 
 
 class Commands:
     def __init__(self, mongodborm, userstorage):
         self.mongodbORM = mongodborm
         self.userStorage = userstorage
+        self.userContext = None
 
     def setupUser(self, username):
         result = self.mongodbORM.get_user_info_from_name(username)
@@ -17,7 +19,8 @@ class Commands:
             self.userStorage.set_awsregion(result["awsregion"])
             self.userStorage.set_awspubkeyname(result["awskeyname"])
         else:
-            print 'User setup failed! Username not found in Database'
+            print 'User setup failed! Username not found in ScaleX Database'
             sys.exit(2)
-
-        # Reset context
+        self.userContext = Context(self.userStorage.get_awssecret,
+                                   self.userStorage.get_awstoken,
+                                   self.userStorage.get_awsregion)
