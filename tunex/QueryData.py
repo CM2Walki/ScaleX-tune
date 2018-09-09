6892172dcf2199bb2610732fbc07b5161d14e831
Walentin Lamonos <walentinlamonos@googlemail.com>
2018-09-09 16:30:11 +0200

    Added launch config handling

--- tunex/QueryData.py
+++ tunex/QueryData.py
@@ -3,5 +3,36 @@
 
 class QueryData:
     @staticmethod
-    def get_launch_configuration():
-        return "test"
+    def create_launch_configuration(client, storage):
+        client.create_launch_configuration(
+            LaunchConfigurationName='tunex-cluster',
+            ImageId='ami-027583e616ca104df',
+            KeyName=storage.set_awspubkeyname(),
+            SecurityGroups=[
+                'sg-tunex',
+            ],
+            ClassicLinkVPCId='',
+            ClassicLinkVPCSecurityGroups=[
+                '',
+            ],
+            UserData='string',
+            InstanceId='string',
+            InstanceType='t2.micro',
+            KernelId='',
+            RamdiskId='',
+            BlockDeviceMappings=[
+                {
+                    'DeviceName': '/dev/sda1',
+                    'Ebs': {
+                        'SnapshotId': 'snap-0a9bff332c1cc9f5a',
+                        'VolumeSize': 8,
+                        'VolumeType': 'gp2',
+                        'DeleteOnTermination': True,
+                    },
+                },
+            ],
+            InstanceMonitoring={
+                'Enabled': False
+            },
+            EbsOptimized=False,
+            AssociatePublicIpAddress=True)
