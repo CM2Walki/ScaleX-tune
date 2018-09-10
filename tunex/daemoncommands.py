6b0cc34ef69f4179cf062df3469b95193ff32e23
Walentin Lamonos <walentinlamonos@googlemail.com>
2018-09-10 14:53:43 +0200

    Test

--- tunex/daemoncommands.py
+++ tunex/daemoncommands.py
@@ -27,7 +27,7 @@ class DaemonCommands:
                                            self.userStorage.get_awstoken(),
                                            self.userStorage.get_awsregion())
                 # Retrieve running clusters
-                return result
+                return str(result)
                 response = self.userContext.build_context(self.userStorage)
                 return response
             else:
