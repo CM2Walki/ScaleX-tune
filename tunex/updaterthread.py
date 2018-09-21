3ca356822b8025aca212d0de95952f7089d7bbf7
Walentin Lamonos <walentinlamonos@googlemail.com>
2018-09-21 15:43:12 +0200

    Added latency tracking

--- tunex/updaterthread.py
+++ tunex/updaterthread.py
@@ -33,7 +33,7 @@ class Updater(object):
             if not self.stop:
                 now = datetime.datetime.now()
                 if now.second < 59:
-                    if counter < 5:
+                    if counter > 5:
                         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                         s.settimeout(1)
                         s_start = timer()
